cf2212fa3c34ccdb6ed17b03b1569c902aaab233
Janko Krstic <keichiri@protonmail.com>
2018-07-30 00:04:16 +0200

    header scraping tests

---
+++ code_generator/tests/__init__.py
@@ -0,0 +1,129 @@
+TEST_HEADER_FILE = """
+#ifndef __indy__ledger_included__
+#define __indy__ledger_included__
+
+#include "indy_mod.h"
+#include "indy_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    /// Signs and submits request message to validator pool.
+    ///
+    /// Adds submitter information to passed request json, signs it with submitter
+    /// sign key (see wallet_sign), and sends signed request message
+    /// to validator pool (see write_request).
+    ///
+    /// #Params
+    /// command_handle: command handle to map callback to caller context.
+    /// pool_handle: pool handle (created by open_pool_ledger).
+    /// wallet_handle: wallet handle (created by open_wallet).
+    /// submitter_did: Id of Identity stored in secured Wallet.
+    /// request_json: Request data json.
+    /// cb: Callback that takes command result as parameter.
+    ///
+    /// #Returns
+    /// Request result as json.
+    ///
+    /// #Errors
+    /// Common*
+    /// Wallet*
+    /// Ledger*
+    /// Crypto*
+    
+    extern indy_error_t indy_sign_and_submit_request(indy_handle_t command_handle,
+                                                     indy_handle_t pool_handle,
+                                                     indy_handle_t wallet_handle,
+                                                     const char *  submitter_did,
+                                                     const char *  request_json,
+
+                                                     void           (*cb)(indy_handle_t xcommand_handle,
+                                                                          indy_error_t  err,
+                                                                          const char*   request_result_json)
+                                                     );
+    
+    /// Publishes request message to validator pool (no signing, unlike sign_and_submit_request).
+    ///
+    /// The request is sent to the validator pool as is. It's assumed that it's already prepared.
+    ///
+    /// #Params
+    /// command_handle: command handle to map callback to caller context.
+    /// pool_handle: pool handle (created by open_pool_ledger).
+    /// request_json: Request data json.
+    /// cb: Callback that takes command result as parameter.
+    ///
+    /// #Returns
+    /// Request result as json.
+    ///
+    /// #Errors
+    /// Common*
+    /// Ledger*
+    
+    extern indy_error_t indy_submit_request(indy_handle_t command_handle,
+                                            indy_handle_t pool_handle,
+                                            const char *  request_json,
+
+                                            void           (*cb)(indy_handle_t xcommand_handle,
+                                                                 indy_error_t  err,
+                                                                 const char*   request_result_json)
+                                           );
+
+
+    /// Signs request message.
+    ///
+    /// Adds submitter information to passed request json, signs it with submitter
+    /// sign key (see wallet_sign).
+    ///
+    /// #Params
+    /// command_handle: command handle to map callback to caller context.
+    /// wallet_handle: wallet handle (created by open_wallet).
+    /// submitter_did: Id of Identity stored in secured Wallet.
+    /// request_json: Request data json.
+    /// cb: Callback that takes command result as parameter.
+    ///
+    /// #Returns
+    /// Signed request json.
+    ///
+    /// #Errors
+    /// Common*
+    /// Wallet*
+    /// Ledger*
+    /// Crypto*
+
+    extern indy_error_t indy_sign_request(indy_handle_t command_handle,
+                                         indy_handle_t  wallet_handle,
+                                         const char *   submitter_did,
+                                         const char *   request_json,
+
+                                         void           (*cb)(indy_handle_t xcommand_handle,
+                                                              indy_error_t  err,
+                                                              const char*   signed_request_json)
+                                         );
+                                         
+    typedef uint8_t       indy_u8_t;
+    typedef uint32_t      indy_u32_t;
+    typedef int32_t       indy_i32_t;
+    typedef int32_t       indy_handle_t;
+    typedef unsigned int  indy_bool_t;
+    typedef long long     indy_i64_t;
+    typedef unsigned long long     indy_u64_t;
+
+
+
+"""
+
+TEST_FUNCTION_DECLARATION = """
+indy_error_t indy_sign_request(indy_handle_t command_handle,
+                               indy_handle_t  wallet_handle,
+                               const char *   submitter_did,
+                               const char *   request_json,
+
+                               void           (*cb)(indy_handle_t xcommand_handle,
+                                                    indy_error_t  err,
+                                                    const char*   signed_request_json)
+                               );
+"""
+
+
+TEST_PARAMETERS_STRING = 'indy_handle_t command_handle, indy_handle_t wallet_handle, const char * submitter_did, const char * request_json, void (*cb)(indy_handle_t xcommand_handle, indy_error_t err, const char* signed_request_json)'
---
+++ code_generator/tests/test_header_scraping.py
@@ -0,0 +1,81 @@
+import unittest
+
+from indygo_generator import header_scraping
+from indygo_generator.function import FunctionParameter, CallbackDeclaration
+
+from . import TEST_HEADER_FILE, TEST_FUNCTION_DECLARATION, TEST_PARAMETERS_STRING
+
+
+class HeaderScrapingTests(unittest.TestCase):
+    def test_scrape_header_file(self):
+        declarations, type_map = header_scraping.scrape_header_file(TEST_HEADER_FILE)
+
+        self.assertEqual(len(declarations), 3)
+        declaration_1 = declarations[0]
+        self.assertEqual(declaration_1.name, 'sign_and_submit_request')
+        self.assertEqual(declaration_1.return_type, 'indy_error_t')
+        self.assertEqual(len(declaration_1.parameters), 6)
+        declaration_2 = declarations[1]
+        self.assertEqual(declaration_2.name, 'submit_request')
+        self.assertEqual(declaration_2.return_type, 'indy_error_t')
+        self.assertEqual(len(declaration_2.parameters), 4)
+        declaration_3 = declarations[2]
+        self.assertEqual(declaration_3.name, 'sign_request')
+        self.assertEqual(declaration_3.return_type, 'indy_error_t')
+        self.assertEqual(len(declaration_3.parameters), 5)
+
+        self.assertEqual(len(type_map), 7)
+        self.assertEqual(type_map['indy_u8_t'], 'uint8_t')
+        self.assertEqual(type_map['indy_u32_t'], 'uint32_t')
+        self.assertEqual(type_map['indy_i32_t'], 'int32_t')
+        self.assertEqual(type_map['indy_handle_t'], 'int32_t')
+        self.assertEqual(type_map['indy_bool_t'], 'unsigned int')
+        self.assertEqual(type_map['indy_i64_t'], 'long long')
+        self.assertEqual(type_map['indy_u64_t'], 'unsigned long long')
+
+    def test_parse_function_declaration(self):
+        function_declaration = header_scraping.parse_indy_function_declaration(TEST_FUNCTION_DECLARATION)
+
+        self.assertEqual(function_declaration.name, 'sign_request')
+        self.assertEqual(function_declaration.return_type, 'indy_error_t')
+        self.assertEqual(len(function_declaration.parameters), 5)
+
+    def test_parse_function_parameters(self):
+        parameters = header_scraping._parse_function_parameters(TEST_PARAMETERS_STRING)
+
+        self.assertEqual(len(parameters), 5)
+        self.assertTrue(isinstance(parameters[0], FunctionParameter))
+        self.assertEqual(parameters[0].name, 'command_handle')
+        self.assertEqual(parameters[0].type, 'indy_handle_t')
+        self.assertTrue(isinstance(parameters[1], FunctionParameter))
+        self.assertEqual(parameters[1].name, 'wallet_handle')
+        self.assertEqual(parameters[1].type, 'indy_handle_t')
+        self.assertTrue(isinstance(parameters[2], FunctionParameter))
+        self.assertEqual(parameters[2].name, 'submitter_did')
+        self.assertEqual(parameters[2].type, 'const char *')
+        self.assertTrue(isinstance(parameters[3], FunctionParameter))
+        self.assertEqual(parameters[3].name, 'request_json')
+        self.assertEqual(parameters[3].type, 'const char *')
+        callback = parameters[4]
+        self.assertTrue(isinstance(callback, CallbackDeclaration))
+        self.assertEqual(callback.rtype, 'void')
+        self.assertEqual(len(callback.parameters), 3)
+        self.assertTrue(isinstance(callback.parameters[0], FunctionParameter))
+        self.assertEqual(callback.parameters[0].name, 'xcommand_handle')
+        self.assertEqual(callback.parameters[0].type, 'indy_handle_t')
+        self.assertTrue(isinstance(callback.parameters[1], FunctionParameter))
+        self.assertEqual(callback.parameters[1].name, 'err')
+        self.assertEqual(callback.parameters[1].type, 'indy_error_t')
+        self.assertTrue(isinstance(callback.parameters[2], FunctionParameter))
+        self.assertEqual(callback.parameters[2].name, 'signed_request_json')
+        self.assertEqual(callback.parameters[2].type, 'const char*')
+
+    def test_split_params(self):
+        param_strings = header_scraping._split_parameter_string(TEST_PARAMETERS_STRING)
+
+        self.assertEqual(len(param_strings), 5)
+        self.assertEqual(param_strings[0], 'indy_handle_t command_handle')
+        self.assertEqual(param_strings[1], 'indy_handle_t wallet_handle')
+        self.assertEqual(param_strings[2], 'const char * submitter_did')
+        self.assertEqual(param_strings[3], 'const char * request_json')
+        self.assertEqual(param_strings[4], 'void (*cb)(indy_handle_t xcommand_handle, indy_error_t err, const char* signed_request_json)')
